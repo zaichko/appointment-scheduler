7ddfc698918e5d610734fc22efd3ae74b530e808
Yekaterina Zaichko <245034547+zaichko@users.noreply.github.com>
2026-08-29 22:13:22 +0500

    add missing enums

---
+++ src/main/java/com/zaichko/scheduler/enums/AppointmentStatus.java
@@ -0,0 +1,8 @@
+package com.zaichko.scheduler.enums;
+
+public enum AppointmentStatus {
+    SCHEDULED,
+    COMPLETED,
+    CANCELED,
+    NO_SHOW
+}
---
+++ src/main/java/com/zaichko/scheduler/enums/Role.java
@@ -0,0 +1,7 @@
+package com.zaichko.scheduler.enums;
+
+public enum Role {
+    PATIENT,
+    DOCTOR,
+    ADMIN
+}
